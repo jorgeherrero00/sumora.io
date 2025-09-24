4a97276b79389c26bd9c85861668f542c903ab12
Jorge Herrero <herrerojorge730@gmail.com>
2025-09-24 11:29:37 +0200

    fix: Correct allowed_origins in CORS configuration for better security

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*', 'chrome-extension://*'],
+    'allowed_origins' => ['chrome-extension://cnaolnnhjihnondfkbmciiibachmepcn', 'https:://sumora.herustudio.com'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
