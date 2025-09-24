52d375125e4a71f22a6989562c83fff051043649
Jorge Herrero <herrerojorge730@gmail.com>
2025-09-24 10:38:16 +0200

    feat: Restore ping route for health check in API

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MeetingController;
+Route::get('/ping', fn() => response()->json(['pong' => true]));
 
 Route::middleware('auth.api_token')->post('/upload', [MeetingController::class, 'uploadFromExtension']);
